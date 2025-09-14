36d4e3c9777ba3bf1ed7480ec0a7ec045ced4646
Calypso694 <78108542+Calypso694@users.noreply.github.com>
2025-09-13 20:33:34 -0700

    Add files via upload

---
+++ C part 2/Server/server.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/*
+Create a socket using the socket() function.
+Bind the socket to an address using the bind() function.
+Listen for connections with the listen() function.
+Accept a connection with the accept() function.
+Send and receive data.
+Remember to handle errors and close the socket when you're done.
+*/
+int main void()
+{
+    return 0;
+}
